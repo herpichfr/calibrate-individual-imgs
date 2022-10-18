6a87ecc2c5fe61b8b3df5cc2ce311ed6febdf53f
herpichfr <Fabio R. Herpich>
2022-10-17 19:55:19 -0700

    Adding feature to not redone images already processed

--- main.py
+++ main.py
@@ -18,96 +18,100 @@ def calibrate_indimgs(tab, imgs):
     """calibrate individual frames used for SPLUS MS coadding"""
     for img in imgs:
         print('calculating for image', img)
-        image_mask = tab[1].data['exposure_id'] == img
-        filtername = np.unique(tab[1].data['filter'][image_mask])
-        print('filter is', filtername.item())
-        if filtername.size > 1:
-            raise IOError('duplicated filter for image', img)
-        tiles = np.array([i[:-7].replace('_', '-') for i in tab[1].data['obs_id']])
-        tile = np.unique(tiles[image_mask]).item()
-        print('tile is', tile)
-
-        # get calibrated table of the tile of the image
-        mstabname = '/storage/splus/Catalogues/asteroids/idr4_query_' + tile + '.csv'
-        print('reading MS tab', mstabname)
-        mstab = pd.read_csv(mstabname)
-        mstab.rename(columns={'u_auto': 'U', 'J0378_auto': 'F378', 'J0395_auto': 'F395',
-                              'J0410_auto': 'F410', 'J0430_auto': 'F430', 'g_auto': 'G',
-                              'J0515_auto': 'F515', 'r_auto': 'R', 'J0660_auto': 'F660',
-                              'i_auto': 'I', 'J0861_auto': 'F861', 'z_auto': 'Z'},
-                     inplace=True)
-
-        # create the sky coordinates for both tables and match them
-        print('preparing coordinates for matching')
-        c1 = SkyCoord(ra=tab[1].data['ra'][image_mask], dec=tab[1].data['dec'][image_mask], unit=(u.deg, u.deg))
-        c2 = SkyCoord(ra=mstab['RA'], dec=mstab['DEC'], unit=(u.deg, u.deg))
-        print('matching image', img, 'with table', mstabname)
-        idx, d2d, d3d = c2.match_to_catalog_sky(c1)
-        max_sep = 1.0 * u.arcsec
-        sep_constraint = d2d < max_sep
-
-        # finding ZP of image
-        mask = (mstab[filtername.item()] > 14) & (mstab[filtername.item()] < 18)
-        a = mstab[filtername.item()][sep_constraint & mask] - tab[1].data['mag'][image_mask][idx][sep_constraint & mask]
-        zp = np.median(a)
-        percs = np.percentile(a, [16, 84, 0.05, 99.95])
-        num_obj = a.size
-        print('ZP for image', img, 'filter', filtername.item(), 'field', tile, 'is', zp)
-
-        # applying correction to individual catalogue
-        print('calibrating image', img, 'for filter', filtername.item(), 'and field', tile)
-        newt = Table(data=tab[1].data[image_mask], names=tab[1].data.columns.names)
-        newt['mag'] += zp
-        imgtabname = '/storage/splus/Catalogues/asteroids/indImgsDiag/' + img + '_phot.csv'
-        newt.to_pandas().to_csv(imgtabname, index=False)
-
-        # save diagnostic things
-        print('saving table with the params')
-        save_tabs4imgs(img, tile, zp, percs, num_obj)
-
-        print('saving diagnostic figure', img + '_diag.png')
-        # plot_diagnostics(img, a, zp, percs, num_obj, c1, c2,
-        #                 mstab[filtername.item()][sep_constraint & mask], sample=sep_constraint & mask)
-        fig = plt.figure()
-        ax1 = fig.add_subplot(221)
-        ax1.scatter(c1.ra, c1.dec, marker='o', c='c', s=20, label='ind')
-        ax1.scatter(c2.ra, c2.dec, marker='.', c='k', s=5, label='MS', alpha=0.5)
-        ax1.set_xlabel('RA')
-        ax1.set_ylabel('Dec')
-        ax1.set_title(img + ' f: ' + filtername.item(), fontsize=10)
-        ax1.legend(loc='upper left', fontsize=8)
-
-        ax2 = fig.add_subplot(222)
-        ax2.scatter(c1.ra[idx][sep_constraint & mask], c1.dec[idx][sep_constraint & mask],
-                    marker='o', c='c', s=20, label='ind')
-        ax2.scatter(c2.ra[sep_constraint & mask], c2.dec[sep_constraint & mask],
-                    marker='.', c='k', s=5, label='MS', alpha=0.5)
-        ax2.set_xlabel('RA')
-        ax2.set_title('N = %i' % int(num_obj), fontsize=10)
-        ax2.legend(loc='upper left', fontsize=8)
-
-        ax3 = fig.add_subplot(223)
-        y, x, _ = ax3.hist(a, bins=100, color='r', range=(percs[2], percs[3]))
-        ax3.set_xlabel('mag_auto_MS - mag_auto_ind')
-        ax3.plot([zp, zp], [-0.01, max(y) + 1], '--', c='k', lw=1., label='median: %.2f' % zp)
-        ax3.plot([percs[0], percs[0]], [-0.01, max(y) + 1], '-.', c='k', lw=1.5, label='p16: %.2f' % percs[0])
-        ax3.plot([percs[1], percs[1]], [-0.01, max(y) + 1], '-.', c='k', lw=1.5, label='p84: %.2f' % percs[1])
-        ax3.legend(loc='upper left', fontsize=8)
-
-        ax4 = fig.add_subplot(224)
-        ax4.scatter(mstab[filtername.item()][sep_constraint & mask], a, marker='.', c='b')
-        ax4.plot([min(mstab[filtername.item()][sep_constraint & mask]),
-                  max(mstab[filtername.item()][sep_constraint & mask])],
-                 [zp, zp], '-', c='k', lw=1.5)
-        ax4.set_xlabel('mag_auto_MS')
-        ax4.set_ylabel('MS - ind')
-
-        plt.tight_layout()
-
+        # create path for output image
         dir2save = '/storage/splus/Catalogues/asteroids/indImgsDiag/'
         imgdiagname = dir2save + img + '_diag.png'
-        plt.savefig(imgdiagname, format='png', dpi=120)
-        plt.close()
+        if os.path.isfile(imgdiagname):
+            print('Image', img, 'already done! Passing...')
+        else:
+            image_mask = tab[1].data['exposure_id'] == img
+            filtername = np.unique(tab[1].data['filter'][image_mask])
+            print('filter is', filtername.item())
+            if filtername.size > 1:
+                raise IOError('duplicated filter for image', img)
+            tiles = np.array([i[:-7].replace('_', '-') for i in tab[1].data['obs_id']])
+            tile = np.unique(tiles[image_mask]).item()
+            print('tile is', tile)
+
+            # get calibrated table of the tile of the image
+            mstabname = '/storage/splus/Catalogues/asteroids/idr4_query_' + tile + '.csv'
+            print('reading MS tab', mstabname)
+            mstab = pd.read_csv(mstabname)
+            mstab.rename(columns={'u_auto': 'U', 'J0378_auto': 'F378', 'J0395_auto': 'F395',
+                                  'J0410_auto': 'F410', 'J0430_auto': 'F430', 'g_auto': 'G',
+                                  'J0515_auto': 'F515', 'r_auto': 'R', 'J0660_auto': 'F660',
+                                  'i_auto': 'I', 'J0861_auto': 'F861', 'z_auto': 'Z'},
+                         inplace=True)
+
+            # create the sky coordinates for both tables and match them
+            print('preparing coordinates for matching')
+            c1 = SkyCoord(ra=tab[1].data['ra'][image_mask], dec=tab[1].data['dec'][image_mask], unit=(u.deg, u.deg))
+            c2 = SkyCoord(ra=mstab['RA'], dec=mstab['DEC'], unit=(u.deg, u.deg))
+            print('matching image', img, 'with table', mstabname)
+            idx, d2d, d3d = c2.match_to_catalog_sky(c1)
+            max_sep = 1.0 * u.arcsec
+            sep_constraint = d2d < max_sep
+
+            # finding ZP of image
+            mask = (mstab[filtername.item()] > 14) & (mstab[filtername.item()] < 18)
+            a = mstab[filtername.item()][sep_constraint & mask] - tab[1].data['mag'][image_mask][idx][sep_constraint & mask]
+            zp = np.median(a)
+            percs = np.percentile(a, [16, 84, 0.05, 99.95])
+            num_obj = a.size
+            print('ZP for image', img, 'filter', filtername.item(), 'field', tile, 'is', zp)
+
+            # applying correction to individual catalogue
+            print('calibrating image', img, 'for filter', filtername.item(), 'and field', tile)
+            newt = Table(data=tab[1].data[image_mask], names=tab[1].data.columns.names)
+            newt['mag'] += zp
+            imgtabname = '/storage/splus/Catalogues/asteroids/indImgsDiag/' + img + '_phot.csv'
+            newt.to_pandas().to_csv(imgtabname, index=False)
+
+            # save diagnostic things
+            print('saving table with the params')
+            save_tabs4imgs(img, tile, zp, percs, num_obj)
+
+            print('saving diagnostic figure', img + '_diag.png')
+            # plot_diagnostics(img, a, zp, percs, num_obj, c1, c2,
+            #                 mstab[filtername.item()][sep_constraint & mask], sample=sep_constraint & mask)
+            fig = plt.figure()
+            ax1 = fig.add_subplot(221)
+            ax1.scatter(c1.ra, c1.dec, marker='o', c='c', s=20, label='ind')
+            ax1.scatter(c2.ra, c2.dec, marker='.', c='k', s=5, label='MS', alpha=0.5)
+            ax1.set_xlabel('RA')
+            ax1.set_ylabel('Dec')
+            ax1.set_title(img + ' f: ' + filtername.item(), fontsize=10)
+            ax1.legend(loc='upper left', fontsize=8)
+
+            ax2 = fig.add_subplot(222)
+            ax2.scatter(c1.ra[idx][sep_constraint & mask], c1.dec[idx][sep_constraint & mask],
+                        marker='o', c='c', s=20, label='ind')
+            ax2.scatter(c2.ra[sep_constraint & mask], c2.dec[sep_constraint & mask],
+                        marker='.', c='k', s=5, label='MS', alpha=0.5)
+            ax2.set_xlabel('RA')
+            ax2.set_title('N = %i' % int(num_obj), fontsize=10)
+            ax2.legend(loc='upper left', fontsize=8)
+
+            ax3 = fig.add_subplot(223)
+            y, x, _ = ax3.hist(a, bins=100, color='r', range=(percs[2], percs[3]))
+            ax3.set_xlabel('mag_auto_MS - mag_auto_ind')
+            ax3.plot([zp, zp], [-0.01, max(y) + 1], '--', c='k', lw=1., label='median: %.2f' % zp)
+            ax3.plot([percs[0], percs[0]], [-0.01, max(y) + 1], '-.', c='k', lw=1.5, label='p16: %.2f' % percs[0])
+            ax3.plot([percs[1], percs[1]], [-0.01, max(y) + 1], '-.', c='k', lw=1.5, label='p84: %.2f' % percs[1])
+            ax3.legend(loc='upper left', fontsize=8)
+
+            ax4 = fig.add_subplot(224)
+            ax4.scatter(mstab[filtername.item()][sep_constraint & mask], a, marker='.', c='b')
+            ax4.plot([min(mstab[filtername.item()][sep_constraint & mask]),
+                      max(mstab[filtername.item()][sep_constraint & mask])],
+                     [zp, zp], '-', c='k', lw=1.5)
+            ax4.set_xlabel('mag_auto_MS')
+            ax4.set_ylabel('MS - ind')
+
+            plt.tight_layout()
+
+            plt.savefig(imgdiagname, format='png', dpi=120)
+            plt.close()
 
     return
 
